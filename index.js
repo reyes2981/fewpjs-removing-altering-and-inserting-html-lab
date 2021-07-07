1d2181acc0521e45b6c4d8c82721649a118afb8c
noe <noe.reyes92@yahoo.com>
2021-07-07 11:29:41 -0500

    commit tree e8bd2d92b56991a5f6a3a74d1bc10393bc96c8a6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector("#main").remove();
+
+var newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "NOE is the champion";
